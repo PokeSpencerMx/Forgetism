17ffb5996c6853cc75466300f924dafcc4371195
madi-smith <mesmith10@quinnipiac.edu>
2023-11-12 21:21:37 -0500

    portal

---
+++ Assets/Scripts/Portal.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Portal : MonoBehaviour
+{
+
+    public float x;
+    public float y;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            collision.transform.position = new Vector2(x, y);
+        }
+    }
+
+
+}
