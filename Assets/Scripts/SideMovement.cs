5b4ca51a52cd9293625f2c54e872462f29c47205
Nate Pippin <nrpippin@quinnipiac.edu>
2023-11-14 21:02:45 -0500

    animation stuff

--- Assets/Scripts/SideMovement.cs
+++ Assets/Scripts/SideMovement.cs
@@ -42,13 +42,17 @@ public class SideMovement : MonoBehaviour
             if (Input.GetKey(KeyCode.LeftShift))
             {
                 //Animation for getting ready to forget a direction
+                animator.SetBool("thinking", true);
 
 
                 if (Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.A))
                 {
                     // Debug.Log("Forgot how to move left");
-                    
-                    //Animation for forgetting left should go here.
+
+                    //Animation for forgetting left should go here. 
+                    //animator.SetBool("thinking", false);
+                    //animator.SetBool("unmovable", true);
+                   // animator.SetBool("right", true);
 
                     //losingLeft.Invoke();
                     gonnaLoseLeft = true;
@@ -56,22 +60,33 @@ public class SideMovement : MonoBehaviour
                     {
                         Instantiate(leftPrefab, new Vector3(transform.position.x - 0.1f, transform.position.y - 0.2f, leftPrefab.transform.position.z), leftPrefab.transform.rotation);
                         FMODUnity.RuntimeManager.PlayOneShot("event:/Forgetism_Gumball_Drop/Gumball_Drop");
+                        //animator.SetBool("unmovable", false);
+                        //animator.SetBool("right", false);
 
                     }
                     
                 }
                 if (Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.D))
                     {
-                        // Debug.Log("Forgot how to move right");
-                        //Animation for forgetting right should go here.
+                    // Debug.Log("Forgot how to move right");
+                    //Animation for forgetting right should go here.
+                    //animator.SetBool("thinking", false);
+                    //animator.SetBool("unmovable", true);
+                    //animator.SetBool("right", true);
 
-                        gonnaLoseRight = true;
+                    gonnaLoseRight = true;
                         if (noRight == false)
                         {
                             Instantiate(rightPrefab, new Vector3(transform.position.x, transform.position.y, rightPrefab.transform.position.z), rightPrefab.transform.rotation);
                             FMODUnity.RuntimeManager.PlayOneShot("event:/Forgetism_Gumball_Drop/Gumball_Drop");
-                        }
+                           // animator.SetBool("unmovable", false);
+                           // animator.SetBool("right", false);
                     }
+                    }
+            }
+            else
+            {
+                animator.SetBool("thinking", false);
             }
             transform.position = Vector3.MoveTowards(transform.position, point.position, moveSpeed * Time.deltaTime);
 
@@ -84,11 +99,13 @@ public class SideMovement : MonoBehaviour
                     {
                         canMove = 0;
                         FMODUnity.RuntimeManager.PlayOneShot("event:/Forgetism_Error/Error");
+                        //animator.Play("ForgetLeft");
                     }
                     else if (Input.GetAxisRaw("Horizontal") > 0 && noRight)
                     {
                         canMove = 0;
                         FMODUnity.RuntimeManager.PlayOneShot("event:/Forgetism_Error/Error");
+                        //animator.Play("ForgetRight");
                     }
                     else
                     {
