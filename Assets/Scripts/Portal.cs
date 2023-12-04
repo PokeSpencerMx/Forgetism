70272000a269e2255e32c4c7ac1a140e5c2cbc45
madi-smith <mesmith10@quinnipiac.edu>
2023-12-03 21:41:00 -0500

    two way portal

--- Assets/Scripts/Portal.cs
+++ Assets/Scripts/Portal.cs
@@ -5,16 +5,56 @@ using UnityEngine;
 public class Portal : MonoBehaviour
 {
 
-    public float x;
-    public float y;
+    private float portalTimer;
+    private bool portalActive;
+    private float delay = 2f;
+    public Transform portal;
+    private GameObject gumDum;
+
+    private void Start()
+    {
+        portalTimer = delay;
+    }
+
+    private void Update()
+    {
+        if (gumDum && portalActive)
+        {
+            if(portalTimer > 0)
+            {
+                portalTimer -= Time.deltaTime;
+            } else
+            {
+                Teleport();
+            }
+        }
+    }
+
+    private void Teleport()
+    {
+        gumDum.transform.position = portal.position;
+        gumDum.GetComponent<SideMovement>().ResetPosition();
+        gumDum.GetComponent<VerticalMovement>().ResetPosition();
+        portalTimer = delay;
+        portalActive = false;
+       // gumDum = null;
+    }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
         {
-            collision.transform.position = new Vector2(x, y);
+            portalActive = true;
+            gumDum = collision.gameObject;
         }
     }
-
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            portalActive = false;
+            gumDum = null;
+        }
+    }
 
 }
